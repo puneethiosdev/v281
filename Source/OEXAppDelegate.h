6562ea6c575ad3aa08ac0804a8c20d7261b09fc5
=balakrishnajarugubelli <krishnakanth.jb@tcs.com>
2016-09-14 13:04:34 +0530

    App delegate : Declaration of Global Mailcomposer view controller, VPN check

--- Source/OEXAppDelegate.h
+++ Source/OEXAppDelegate.h
@@ -1,13 +1,15 @@
-//
-//  OEXAppDelegate.h
-//  edXVideoLocker
-//
-//  Created by Nirbhay Agarwal on 15/05/14.
-//  Copyright (c) 2014-2016 edX. All rights reserved.
-//
+    //
+    //  OEXAppDelegate.h
+    //  edXVideoLocker
+    //
+    //  Created by Nirbhay Agarwal on 15/05/14.
+    //  Copyright (c) 2014-2016 edX. All rights reserved.
+    //
 
 @import UIKit;
+@import MessageUI;
 #import "Reachability.h"
+#import "UIWindow+TopController.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -16,6 +18,9 @@ NS_ASSUME_NONNULL_BEGIN
 @interface OEXAppDelegate : UIResponder
 
 @property (nonatomic, strong) id <Reachability> reachability;
+@property MFMailComposeViewController *appliedxMailVC;
+    //KAMAT_CHANGES
+- (void) performVPNAvailability;
 
 - (void)callCompletionHandlerForSession:(NSString*)identifier;
 
